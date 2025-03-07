f0b8797241ba066f2dd4c4e1901671c632a83f0c
marian kopriva <mariankopriva@gmail.com>
2025-03-07 10:51:03 +0100

    Change needed to create a new version.

--- httpdoc/build.go
+++ httpdoc/build.go
@@ -1016,13 +1016,14 @@ func (c *build) _newFieldList(typ *types.Type, aElem *page.ArticleElement, opts 
 		}
 
 		// the field type's enum values
-		if ftype.HasConstValues() {
+		switch {
+		case ftype.HasConstValues():
 			enumList, err := c.newEnumList(ftype)
 			if err != nil {
 				return nil, err
 			}
 			item.EnumList = enumList
-		} else if ftype.ElemHasConstValues() {
+		case ftype.ElemHasConstValues():
 			enumList, err := c.newEnumList(ftype.Elem)
 			if err != nil {
 				return nil, err
